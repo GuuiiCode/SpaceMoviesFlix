81a33ea49f3f2c16440247ea34b737d0dbec3bd0
GuuiiCode <guilherme.job07@gmail.com>
2022-07-21 22:37:05 -0300

    commit parcial

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+const botao_menu = document.getElementsByClassName('cabecalho__menu')[0];
+
+const navegacao = document.getElementsByClassName('cabecalho__navegacao')[0];
+
+botao_menu.addEventListener('click', () => {
+    navegacao.classList.toggle('ativado');
+});
+
+
+
+
+
+
