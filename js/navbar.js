b2f9cd0b9bc0489f4f458a716601ee26691af3db
GuuiiCode <guilherme.job07@gmail.com>
2022-07-26 12:05:24 -0300

    Merge branch 'feature/navbar-dropdown', remote-tracking branch 'origin'

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,41 @@ const botao_menu = document.getElementsByClassName('cabecalho__menu')[0];
 
 const navegacao = document.getElementsByClassName('cabecalho__navegacao')[0];
 
+criarMenus();
+
 botao_menu.addEventListener('click', () => {
     navegacao.classList.toggle('ativado');
 });
 
+
+function criarMenus() {
+
+    let menus = ['Filmes', 'Séries', 'Animes'];
+
+    let subMenus = ['Ver todos', "Adicionar"];
+
+
+    for (const menu of menus) {
+        let navegacao__link = document.createElement('li');
+        navegacao__link.classList.add('navegacao__link');
+        navegacao__link.innerHTML = menu;
+
+        let ul = document.createElement('ul');
+        navegacao__link.appendChild(ul);
+
+        for (const links of subMenus) {
+            let subMenu = document.createElement('li');
+            subMenu.classList.add('submenu');
+            ul.appendChild(subMenu);
+
+            let a = document.createElement('a');
+            a.href = "https://cssgradient.io/";
+            a.innerHTML = links;
+            subMenu.appendChild(a);
+        }
+
+        navegacao.appendChild(navegacao__link);
+    }
+
+    console.log(navegacao);
+}
